8e4ccd815390122def883e8c281383d41b23200f
Olga Fomin <olga.fomin26@gmail.com>
2019-11-30 18:38:09 -0800

    Add functionality to display tasks

--- App.js
+++ App.js
@@ -2,21 +2,32 @@ import React from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-    const [task, setTask] = React.useState("");
+    const [newTask, setTask] = React.useState("");
+    const [allTasks, setAllTasks] = React.useState([]);
+
+    const onChangeText = text => setTask(text);
+
+    const addTask = () => {
+        if (!newTask.length) return;
+        setAllTasks(currentTasks => [...currentTasks, newTask]);
+        setTask("");
+    };
 
     return (
         <View style={styles.container}>
-            <View>
+            <View style={styles.row}>
                 <TextInput
-                    onChangeText={text => setTask(text)}
-                    value={task}
+                    onChangeText={onChangeText}
+                    value={newTask}
                     placeholder="New task"
                     style={styles.userInput}
                 />
-                <Button title="ADD" />
+                <Button title="ADD" onPress={addTask} />
             </View>
             <View>
-                <Text>{task}</Text>
+                {allTasks.map((task, i) => (
+                    <Text key={task + i}>{task}</Text>
+                ))}
             </View>
         </View>
     );
@@ -26,7 +37,17 @@ const styles = StyleSheet.create({
     container: {
         padding: 30
     },
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    btn: {
+        width: "20%",
+        color: "black"
+    },
     userInput: {
+        width: "80%",
         padding: 10,
         borderColor: "black",
         borderWidth: 1
